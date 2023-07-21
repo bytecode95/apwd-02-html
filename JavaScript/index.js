c82c3e2f23536abdcfb3ddd6bd918a6b3e9972e1
chethanavirajini <cheth.virajini@gmail.com>
2023-07-21 20:21:44 +0530

    Global and local variables

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,3 +4,13 @@ console.log("external java script")
 // 1. var
 // 2. let
 // 3. const
+
+// global variable and local variable
+
+var name= "Chethana"  //global variables
+Check()
+function Check(){
+    var name = "Virajini"   //local variable
+    document.write(name)
+}
+document.write(name)
