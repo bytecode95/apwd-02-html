0230707b01317d80d229824d7113645d2c0a6162
chethanavirajini <cheth.virajini@gmail.com>
2023-07-23 08:53:42 +0530

    add function

---
+++ Operators/function.js
@@ -0,0 +1,42 @@
+console.log("run");
+
+// console.log("Go to home...");
+// console.log("Go to school...");
+
+// printOne();
+printTwo();
+printOne();
+printOne();
+printOne();
+printOne();
+printOne();
+printOne();
+printOne();
+printOne();
+printOne();
+printOne();
+
+maths();
+total(15,25)
+total(476,125)
+
+function printOne(){
+    console.log("Go to home...");
+}
+
+function printTwo(){
+    console.log("Go to school...");
+}
+
+function maths(){
+    let one = 10;
+    let two = 20;
+    let total = one + two
+    console.log(total)
+}
+
+
+function total(one, two){
+    let total = one + two
+    console.log(total)
+}
