22c4c2af7cda16e98348b7d3904c520254f5f7ca
chethanavirajini <cheth.virajini@gmail.com>
2023-07-23 09:12:47 +0530

    add return function

--- Operators/function.js
+++ Operators/function.js
@@ -16,9 +16,7 @@ printOne();
 printOne();
 printOne();
 
-maths();
-total(15,25)
-total(476,125)
+console.log('........................')
 
 function printOne(){
     console.log("Go to home...");
@@ -28,6 +26,24 @@ function printTwo(){
     console.log("Go to school...");
 }
 
+
+maths();
+total(15,25)
+total(476,125)
+
+
+// maths2023(5,2) for return type function does not print directly its results
+const result = maths2023(2, 5)
+console.log(result);
+
+
+const result1 = maths2023(60, 5)
+console.log(result1);
+
+console.log(maths2023(10,8))
+
+
+
 function maths(){
     let one = 10;
     let two = 20;
@@ -35,8 +51,18 @@ function maths(){
     console.log(total)
 }
 
+//print function values
 
 function total(one, two){
     let total = one + two
     console.log(total)
 }
+
+
+//return function
+
+function maths2023(one, two){
+    let total = one * two
+    return total;
+}
+
